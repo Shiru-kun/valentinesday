243fa070011c95895a6335166398dc68c8470e8a
Focas Kandulo <focasdk@gmail.com>
2023-02-14 02:49:36 +0200

    add : Valentines

---
+++ app.js
@@ -0,0 +1,4 @@
+// const name = prompt("Enter your name:");
+// const image = prompt("Enter the image URL:");
+document.getElementById("name").innerHTML = `Te amo Mira Alex!!`;
+// document.getElementById("image").src = image;
