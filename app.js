d30e55456bf9642cc24e15af258dc25d4fd6b77b
Focas Kandulo <focasdk@gmail.com>
2023-02-14 04:50:58 +0200

    lazy loading for the images

--- app.js
+++ app.js
@@ -4,3 +4,40 @@
 // document.getElementById("photo").src = photo;
 document.getElementById("message").innerHTML = "Mira Alex";
 document.getElementById("sender").innerHTML = "By: FK";
+
+const images = document.querySelectorAll('img[data-src]');
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      const img = entry.target;
+      img.src = img.dataset.src;
+      observer.unobserve(img);
+    }
+  });
+});
+
+images.forEach(img => observer.observe(img));
+
+
+const elementsWithBg = document.querySelectorAll('.flower-bg');
+const bgImages = Array.from(elementsWithBg).map(element => {
+  const style = window.getComputedStyle(element);
+  return style.backgroundImage.slice(4, -1).replace(/["']/g, "");
+});
+
+Promise.all(bgImages.map(url => {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.src = url;
+    img.onload = () => resolve(url);
+    img.onerror = reject;
+  });
+})).then(() => {
+  const style = document.createElement('style');
+  style.innerHTML = bgImages.reduce((acc, url, i) => {
+    return acc + `.flower-bg:nth-child(${i + 1}) { background-image: url(${url}) }`;
+  }, "");
+  document.head.appendChild(style);
+});
+
